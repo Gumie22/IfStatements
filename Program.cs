d0fc6e960ed65ae88392338da014b03b1d8a4c9f
Gumani Ratshisuka <62070830+Gumie22@users.noreply.github.com>
2020-10-30 15:42:36 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿//This is a C# program exploring if statements and booleans
+
+using System;
+
+namespace program_2.cs
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            bool isMale = false;
+            bool isTall = false;
+
+            if (isMale && isTall)
+            {
+                Console.WriteLine("You are a tall male");
+            }
+            else if (isMale && !isTall)
+            {
+                Console.WriteLine("You are a short male");
+            }
+            else if (!isMale && isTall)
+            {
+                Console.WriteLine("You are not a male but you are tall");
+            }
+            else
+            {
+                Console.WriteLine("Your are neither male nor tall");
+            }
+        }
+    }
+}
